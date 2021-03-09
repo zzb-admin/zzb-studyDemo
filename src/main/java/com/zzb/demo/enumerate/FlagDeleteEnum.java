37f8386afebeb51c408b235c2958d5a5b492a293
zhaozhengbo <1433755991@qq.com>
2021-03-09 15:09:32 +0800

    代码调整

---
+++ src/main/java/com/zzb/demo/enumerate/FlagDeleteEnum.java
@@ -0,0 +1,46 @@
+package com.zzb.demo.enumerate;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * @author monkey
+ */
+@AllArgsConstructor
+public enum FlagDeleteEnum {
+
+    NO(1,"未删除"),
+    YES(2,"已删除"),
+    UN_KNOWN(0,"未知");
+
+    public Integer code;
+    public String msg;
+
+    private static Map<Integer,FlagDeleteEnum> maps = Maps.newHashMap();
+
+    static{
+        for (FlagDeleteEnum flagDeleteEnum : FlagDeleteEnum.values()) {
+            maps.put(flagDeleteEnum.getCode(),flagDeleteEnum);
+        }
+    }
+
+    public static FlagDeleteEnum of(Integer code){
+        if(null == maps.get(code)){
+            return FlagDeleteEnum.UN_KNOWN;
+        }
+        return maps.get(code);
+    }
+
+
+    public Integer getCode(){
+        return code;
+    }
+
+    @JsonValue
+    public String getMsg(){
+        return msg;
+    }
+}
